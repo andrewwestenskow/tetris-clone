5ddd63254b60bdab588ce41328fa8a7b084264a6
Andrew Westenskow <andrew.westenskow@gmail.com>
2019-09-24 12:25:08 -0600

    Basic movement

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
 const gameWindow = document.getElementById('game-window')
+let rotate = 90
+let leftPos = 200
+let topPos = 0
+let bottomLimit = 760
+
+const handleKeyDown = (e) => {
+  const active = document.querySelector('.active')
+  switch (e.code) {
+    case 'ArrowLeft':
+      if (leftPos !== 0) {
+        leftPos -= 40
+        active.style.left = `${leftPos}px`
+      }
+      break;
+    case 'ArrowRight':
+      if (leftPos !== 360) {
+        leftPos += 40
+        active.style.left = `${leftPos}px`
+      }
+      break;
+    case 'ArrowDown':
+      if (topPos !== bottomLimit) {
+        topPos += 40
+        active.style.top = `${topPos}px`
+      } else {
+        active.classList.remove('active')
+      }
+      break;
+    case 'Space':
+      active.style.transform = `rotate(${rotate}deg)`
+      rotate += 90
+      break;
+  }
+}
+
+window.addEventListener('keydown', handleKeyDown)
 
